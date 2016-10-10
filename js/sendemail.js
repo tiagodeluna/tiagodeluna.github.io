1c3dc1edca044266ba2610f5dc328930e8969d4d
Tiago Luna <tiagodeluna@hotmail.com>
2016-10-10 00:07:36 -0300

    Updating domain for sending mail

--- js/sendemail.js
+++ js/sendemail.js
@@ -27,7 +27,7 @@
                 if(validateForm()){
                  e.preventDefault();
                   $.ajax({type: "POST",
-                          url: "http://izee.ro/envato/mywall/one/send-email.php",
+                          url: "http://tiagodeluna.github.io/send-email.php",
                           data:$("#ContactForm").serialize(),
                           success:function(result){
                           $("#successmsg").html(result);
